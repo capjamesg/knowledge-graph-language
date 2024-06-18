3ff8432f1de6b7219da068e604769f4c7e01a84b
capjamesg <jamesg@jamesg.blog>
2024-06-18 20:12:35 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,36 @@
-from setuptools import setup
+import setuptools
+from setuptools import find_packages
+import re
 
-setup(
+with open("./kgl/__init__.py", 'r') as f:
+    content = f.read()
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="kgl",
-    version="0.1.2",
-    author="James (capjamesg)",
+    version=version,
+    author="capjamesg",
+    author_email="jamesg@jamesg.blog",
     description="Knowledge Graph Language (KGL) parser.",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    python_requires='>=3.8',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/capjamesg/knowledge-graph-language",
     install_requires=[
         "lark==0.11.3",
         "numpy<2.0",
         "faiss-cpu"
     ],
-    keywords=["knowledge graph", "graphs"],
-    license="MIT License",
+    packages=find_packages(exclude=("tests",)),
+    extras_require={
+        "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel", "tox", "responses"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.7",
 )
